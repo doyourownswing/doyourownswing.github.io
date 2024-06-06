eb489f375caa5ced5c51a8bc115bf09a5519f3ca
Jessee Zhang <jesseelzhang@gmail.com>
2024-06-05 22:08:28 -0700

    Fixes merge conflicts

--- web/src/common/theme.js
+++ web/src/common/theme.js
@@ -31,22 +31,36 @@ const theme = responsiveFontSizes(
         subtitleLight: "#bbbbbb",
       },
     },
-    typography: {
-      fontFamily: [
-        "Nunito",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        '"Segoe UI"',
-        "Roboto",
-        '"Helvetica Neue"',
-        "Arial",
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(","),
+    background: {
+      light: "white",
+      lightGray: "#ededed",
+      darkPurple: "#260B34",
     },
-  })
-);
+    text: {
+      titleLight: "white",
+      subtitleLight: "#bbbbbb",
+      subtitle: "#808080",
+      // For less essential body text that we don't want to stand out as much.
+      secondaryBody: "#666666",
+      textButton: "#955ab5",
+      activeSelection: "#955ab5",
+    },
+  },
+  typography: {
+    fontFamily: [
+      "Nunito",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+));
 
 export default theme;
