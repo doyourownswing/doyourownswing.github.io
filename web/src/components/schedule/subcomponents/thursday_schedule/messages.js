fba7b2fdc59d12d1d0e562a5a94bee73cb01a1e3
Jake Rehwoldt <jakerehwoldt@gmail.com>
2024-07-27 19:18:44 -0700

    Removed redundant subtitle text

--- web/src/components/schedule/subcomponents/thursday_schedule/messages.js
+++ web/src/components/schedule/subcomponents/thursday_schedule/messages.js
@@ -1,6 +1,6 @@
 const messages = {
   title: "Thursday Night Schedule",
-  subtitle: "Studio M opens for DYOS shortly before 7:30pm each Thursday",
+  subtitle: "Studio M opens for DYOS shortly before 7:30pm",
 };
 
 export default messages;
