3e50980ccbb0ff0462707eb4fcd6a807ff1f7630
Jake Rehwoldt <jakerehwoldt@gmail.com>
2024-09-07 13:20:10 -0700

    updated song of the week

--- web/src/data/songs_of_the_week.js
+++ web/src/data/songs_of_the_week.js
@@ -20,6 +20,9 @@ const attributors = {
   makenzie: {
     name: "Makenzie S",
   },
+  jake: {
+    name: "Jake R",
+  },
 };
 
 const songs = [
@@ -63,6 +66,27 @@ const songs = [
         "https://i.scdn.co/image/ab67616d0000b273e90ad23c08b4aa0e3aa8d7a1",
       youtube: "https://www.youtube.com/watch?v=uCmcKuA5r3A",
       spotify: "https://open.spotify.com/track/6q8bIgs9jfJpkM3bJmbDie",
+    }
+  ),
+  new SongOfTheWeek(
+    createDate("09/07/2024"),
+    {
+      quote:
+        "This song has a ton of different layers to dance to, with tons of hits " +
+        "and variation from start to finish. " +
+        "It's groovy and has a ton of energy, it always gets me in the mood to dance!",
+      attributionInfo: attributors.jake,
+    },
+    {
+      name: "BOOM",
+      artist: "X Ambassadors",
+      year: "2019",
+    },
+    {
+      albumCoverImage:
+        "https://i.scdn.co/image/ab67616d00001e02899fac4febbc464371e54c27",
+      youtube: "https://youtu.be/mXLRy1cNMWs?si=x4s1mrCxihOt2a6x",
+      spotify: "https://open.spotify.com/track/3V9cM3nCH2G66afoDi0snu",
     }
   ),
 ];
