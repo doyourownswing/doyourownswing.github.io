10d0e5fb435b657d96e4cb9e489cbc97d1b4874a
Jessee Zhang <jesseelzhang@gmail.com>
2025-03-26 20:09:58 -0700

    Removes commented out code

--- web/src/components/schedule/schedule.js
+++ web/src/components/schedule/schedule.js
@@ -1,12 +1,8 @@
 import { Box } from "@mui/material";
-import ClassesOverview from "./subcomponents/classes_overview/classes_overview";
-import ThursdaySchedule from "./subcomponents/thursday_schedule/thursday_schedule";
 import Classes from "./subcomponents/classes/classes";
 
 const Schedule = () => (
   <Box>
-    {/* <ClassesOverview />
-    <ThursdaySchedule /> */}
     <Classes />
   </Box>
 );
