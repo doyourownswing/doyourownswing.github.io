20bd54422b56db6610861da8346211249b594b21
Jessee Zhang <jesseelzhang@gmail.com>
2025-11-04 00:52:06 -0800

    Creates a winter schedule section

--- web/src/components/home/home.js
+++ web/src/components/home/home.js
@@ -1,13 +1,14 @@
 import { Box } from "@mui/material";
 import Hero from "./subcomponents/hero/hero";
 import SongOfTheWeek from "./subcomponents/song_of_the_week/song_of_the_week";
-import UpcomingEvents from "./subcomponents/upcoming_events/upcoming_events";
+// import UpcomingEvents from "./subcomponents/upcoming_events/upcoming_events";
 import Welcome from "./subcomponents/welcome/welcome";
 import WhatIsWcs from "./subcomponents/what_is_wcs/what_is_wcs";
 import WhoWeAre from "./subcomponents/who_we_are/who_we_are";
 import FeatureFlags from "infra/feature_flags";
 import { useRef } from "react";
 import Merch from "./subcomponents/merch/merch";
+import WinterSchedule from "./subcomponents/winter_schedule_2025/winter_schedule";
 
 export default function Home() {
   const upcomingEventsRef = useRef(null);
@@ -24,7 +25,9 @@ export default function Home() {
       {FeatureFlags.useHeroV2 && (
         <Hero onClickPrimaryButton={onClickPrimaryHeroButton} />
       )}
-      <UpcomingEvents ref={upcomingEventsRef} />
+      {/* TODO update this, obviously */}
+      {/* <UpcomingEvents ref={upcomingEventsRef} /> */}
+      <WinterSchedule ref={upcomingEventsRef} />
       <WhoWeAre />
       <WhatIsWcs />
       <SongOfTheWeek />
---
+++ web/src/components/home/subcomponents/winter_schedule_2025/winter_schedule.js
@@ -0,0 +1,71 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { forwardRef } from "react";
+import winterScheduleStyles from "./winter_schedule.styles";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import data from "./schedule_data";
+import { formatDate } from "utils/date_utils";
+import stars from "assets/images/hanging_stars.png";
+import snowflake from "assets/images/snowflake.png";
+import messages from "./messages";
+
+const DATE_FORMAT = "MMM D";
+
+function Event(props) {
+  let details = props.details;
+
+  let baseStyle = details.hasDyos
+    ? winterScheduleStyles.dyosDay
+    : winterScheduleStyles.noDyos;
+
+  return (
+    <Grid2 xs={1}>
+      <Stack sx={baseStyle.eventCard} direction={"row"}>
+        <Box>
+          <Typography sx={baseStyle.thursday}>{messages.thursday}</Typography>
+          <Typography sx={baseStyle.date} variant="h5">
+            {formatDate(props.details.date, DATE_FORMAT)}
+          </Typography>
+        </Box>
+        <Box>
+          {details.hasDyos && (
+            <Button variant="text" sx={baseStyle.button} href="#/classes">
+              {messages.seeDetails}
+            </Button>
+          )}
+          {!details.hasDyos && (
+            <Typography sx={baseStyle.noDyosText}>{messages.noDyos}</Typography>
+          )}
+        </Box>
+      </Stack>
+    </Grid2>
+  );
+}
+
+const WinterSchedule = forwardRef(function WinterSchedule(_, ref) {
+  return (
+    <Box ref={ref} sx={winterScheduleStyles.container}>
+      <Box component="img" src={stars} sx={winterScheduleStyles.stars}></Box>
+      {winterScheduleStyles.snowflakes.map((snowflakeStyle) => (
+        <Box component="img" src={snowflake} sx={snowflakeStyle}></Box>
+      ))}
+      <Container sx={winterScheduleStyles.contentContainer}>
+        <Typography variant="h3" sx={winterScheduleStyles.title}>
+          {messages.title}
+        </Typography>
+        <Grid2
+          container
+          columns={{ xs: 1, sm: 2, md: 3 }}
+          rowSpacing={{ xs: 2, sm: 4 }}
+          columnSpacing={2}
+          sx={winterScheduleStyles.eventsContainer}
+        >
+          {data.map((details) => (
+            <Event details={details} />
+          ))}
+        </Grid2>
+      </Container>
+    </Box>
+  );
+});
+
+export default WinterSchedule;
