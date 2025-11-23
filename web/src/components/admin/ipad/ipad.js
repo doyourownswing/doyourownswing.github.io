e93f8763da7caa6c3f219274e0e1bfa59fb02b20
Jessee Zhang <jesseelzhang@gmail.com>
2025-11-23 14:06:20 -0800

    Adds skeleton for the IPad view

---
+++ web/src/components/admin/ipad/ipad.js
@@ -0,0 +1,40 @@
+import { Box, Container, Tab, Typography } from "@mui/material";
+import ipadStyles from "./ipad.styles";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { useState } from "react";
+
+const TABS = {
+  REGISTRATION_FORM: "Registration Form",
+  CLASSES: "Classes",
+  PRICING: "Pricing",
+  PAYMENT: "Payment",
+};
+
+function Ipad() {
+  const [selectedTab, setSelectedTab] = useState(TABS.REGISTRATION_FORM);
+
+  const onTabChange = (_, newValue) => {
+    setSelectedTab(newValue);
+  };
+
+  return (
+    <Box sx={ipadStyles.container}>
+      <Container>
+        <Typography>test</Typography>
+        <TabContext value={selectedTab}>
+          <TabList onChange={onTabChange}>
+            {Object.keys(TABS).map((tab) => (
+              <Tab label={TABS[tab]} value={TABS[tab]} />
+            ))}
+          </TabList>
+          <TabPanel value={TABS.REGISTRATION_FORM}>Registration form</TabPanel>
+          <TabPanel value={TABS.CLASSES}>Classes</TabPanel>
+          <TabPanel value={TABS.PRICING}>Pricing</TabPanel>
+          <TabPanel value={TABS.PAYMENT}>Payment</TabPanel>
+        </TabContext>
+      </Container>
+    </Box>
+  );
+}
+
+export default Ipad;
---
+++ web/src/components/admin/ipad/ipad.styles.js
@@ -0,0 +1,9 @@
+import { SECTION_PADDING } from "common/constants";
+
+const ipadStyles = {
+  container: {
+    padding: SECTION_PADDING,
+  },
+};
+
+export default ipadStyles;
