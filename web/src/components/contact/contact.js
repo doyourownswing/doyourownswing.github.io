d93f3aa770f55881ad5c49f1696cd4c600384c89
Jessee Zhang <jesseelzhang@gmail.com>
2025-12-03 00:00:42 -0800

    Makes contact pages pretty enough for v1

--- web/src/components/contact/contact.js
+++ web/src/components/contact/contact.js
@@ -1,54 +1,135 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Snackbar,
+  SvgIcon,
+  Typography,
+} from "@mui/material";
 import contactStyles from "./contact.styles";
 import DyosLink from "components/common/link";
-import { ContentCopy } from "@mui/icons-material";
+import { ContentCopy, Facebook, Instagram } from "@mui/icons-material";
 import EmailIcon from "@mui/icons-material/Email";
+import DiscordIcon from "components/common/icons/discord";
+import { useState } from "react";
+
+function SocialsButton(props) {
+  return (
+    <Button
+      variant="contained"
+      sx={props.sx}
+      href={props.href}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {props.children}
+    </Button>
+  );
+}
 
 function Contact() {
+  const [copiedSnackbarMessage, setCopiedSnackbarMessage] = useState("");
+  const [copiedSnackbarOpen, setCopiedSnackbarOpen] = useState(false);
+
+  const handleCopyEmail = async () => {
+    try {
+      setCopiedSnackbarOpen(false);
+
+      await navigator.clipboard.writeText("doyourownswing@gmail.com");
+      setCopiedSnackbarMessage("Copied!");
+    } catch (err) {
+      setCopiedSnackbarMessage("Error copying email");
+    }
+
+    setCopiedSnackbarOpen(true);
+  };
+
+  const handleCopiedSnackbarClose = () => {
+    setCopiedSnackbarOpen(false);
+  };
+
   return (
     <Box sx={contactStyles.container}>
       <Container>
         <Box sx={contactStyles.contentContainer}>
           <Box sx={contactStyles.emailCard}>
             <EmailIcon sx={contactStyles.embellishment} />
-            <Typography sx={contactStyles.hook}>
+            <Typography variant="h4" sx={contactStyles.hook}>
               Want to get in touch?
             </Typography>
-            <Typography>Send us an email</Typography>
-            <DyosLink>doyourownswing@gmail.com</DyosLink>
-            <Button variant="contained" endIcon={<ContentCopy />}>
-              Copy
-            </Button>
+            <Box sx={contactStyles.emailCtaContainer}>
+              <Typography display="inline" sx={contactStyles.emailCtaText}>
+                Send us an email at
+              </Typography>
+              <DyosLink
+                href="mailto:doyourownswing@gmail.com"
+                sx={contactStyles.email}
+              >
+                doyourownswing@gmail.com
+              </DyosLink>
+              <Button
+                variant="contained"
+                endIcon={<ContentCopy />}
+                sx={contactStyles.emailCopyButton}
+                onClick={handleCopyEmail}
+              >
+                Copy
+              </Button>
+              <Snackbar
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                open={copiedSnackbarOpen}
+                autoHideDuration={3000}
+                onClose={handleCopiedSnackbarClose}
+                message={copiedSnackbarMessage}
+              />
+            </Box>
           </Box>
 
           <Box sx={contactStyles.discordCard}>
-            <EmailIcon sx={contactStyles.embellishment} />
-            <Typography sx={contactStyles.hook}>
+            <Box sx={contactStyles.embellishmentContainer}>
+              <SvgIcon sx={contactStyles.embellishment}>
+                <DiscordIcon />
+              </SvgIcon>
+            </Box>
+            <Typography variant="h4" sx={contactStyles.hook}>
               Want to get the latest updates?
             </Typography>
-            <Button variant="contained" endIcon={<ContentCopy />}>
+            <SocialsButton
+              sx={contactStyles.discordButton}
+              href="https://discord.gg/EwXq7EYS7e"
+            >
               Join the DYOS Discord
-            </Button>
+            </SocialsButton>
           </Box>
 
           <Box sx={contactStyles.facebookCard}>
-            <EmailIcon sx={contactStyles.embellishment} />
-            <Typography sx={contactStyles.hook}>
+            <Box sx={contactStyles.embellishmentContainer}>
+              <Facebook sx={contactStyles.embellishment} />
+            </Box>
+            <Typography variant="h4" sx={contactStyles.hook}>
               Want to know about special events?
             </Typography>
-            <Button variant="contained" endIcon={<ContentCopy />}>
+            <SocialsButton
+              sx={contactStyles.facebookButton}
+              href="https://www.facebook.com/doyourownswing"
+            >
               Follow us on Facebook
-            </Button>
+            </SocialsButton>
           </Box>
 
           <Box sx={contactStyles.instagramCard}>
-            <EmailIcon sx={contactStyles.embellishment} />
-            <Typography sx={contactStyles.hook}>
+            <Box sx={contactStyles.embellishmentContainer}>
+              <Instagram sx={contactStyles.embellishment} />
+            </Box>
+            <Typography variant="h4" sx={contactStyles.hook}>
               Want to see what we're up to?
             </Typography>
-            <Button variant="contained" endIcon={<ContentCopy />}>
+            <SocialsButton
+              sx={contactStyles.instagramButton}
+              href="https://www.instagram.com/doyourownswing"
+            >
               Follow us on Instagram
-            </Button>
+            </SocialsButton>
           </Box>
         </Box>
       </Container>
--- web/src/components/contact/contact.styles.js
+++ web/src/components/contact/contact.styles.js
@@ -3,9 +3,29 @@ import theme from "common/theme";
 
 const baseCard = {
   boxShadow: BOX_SHADOW,
-  padding: "4rem",
+  padding: "3rem 2rem 4rem",
   borderRadius: "3rem",
   position: "relative",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "2rem",
+};
+
+const baseEmbellishment = {
+  color: "white",
+  opacity: "50%",
+  fontSize: "8rem",
+  pointerEvents: "none",
+};
+
+const baseLinkButton = {
+  textTransform: "none",
+  fontFamily: "Nunito",
+  fontSize: "1.15rem",
+  fontWeight: "700",
+  borderRadius: "2rem",
+  padding: "1rem 3rem",
 };
 
 const contactStyles = {
@@ -18,40 +38,95 @@ const contactStyles = {
     gap: "4rem",
   },
   embellishment: {
-    position: "absolute",
-    color: "white",
-    opacity: "15%",
-    right: "20%",
-    fontSize: "8rem",
+    ...baseEmbellishment,
+  },
+  embellishmentWithOpacity: (opacity) => {
+    return { ...baseEmbellishment, opacity: opacity };
   },
   hook: {
     fontFamily: "Poppins",
     fontSize: "2rem",
     fontWeight: "600",
     color: "white",
+    textAlign: "center",
   },
   emailCard: {
     ...baseCard,
     // TODO figure this color out
     backgroundColor: theme.palette.background.darkPurple,
   },
-
+  emailCtaContainer: {
+    display: "flex",
+    flexDirection: { xs: "column", md: "row" },
+    gap: "0.5rem",
+    // Baseline makes it so the text is aligned with the button text.
+    alignItems: { xs: "center", md: "baseline" },
+  },
+  emailCtaText: {
+    color: "white",
+    fontSize: "1.15rem",
+  },
+  email: {
+    fontSize: "1.15rem",
+    color: theme.palette.text.linkLight,
+    textDecoration: "underline",
+    "&:visited": {
+      color: theme.palette.text.linkLight,
+    },
+    "&:hover": {
+      textDecoration: "underline",
+      color: theme.palette.text.linkLightHover,
+    },
+    "&:active": {
+      color: theme.palette.text.linkLightHover,
+    },
+  },
+  emailCopyButton: {
+    textTransform: "none",
+    // fontFamily: "Nunito",
+    fontWeight: "700",
+    // letterSpacing: "0.2px",
+    fontSize: "1.05rem",
+    padding: "0.25rem 1rem",
+  },
   discordCard: {
     ...baseCard,
     // Discord blurple
     backgroundColor: "#5865F2",
   },
-
   facebookCard: {
     ...baseCard,
     backgroundColor: "#1877F2",
   },
-
   instagramCard: {
     ...baseCard,
     // Instagram Red
     // Cool link: https://about.instagram.com/brand/gradient?subpath=gradient
     backgroundColor: "#FF0069",
+  },
+  discordButton: {
+    ...baseLinkButton,
+    color: "#5865F2",
+    backgroundColor: "#e4e7ff",
+    "&:hover": {
+      backgroundColor: "#cfd4ff",
+    },
+  },
+  facebookButton: {
+    ...baseLinkButton,
+    color: "#1877F2",
+    backgroundColor: "#e7f1ff",
+    "&:hover": {
+      backgroundColor: "#d4e6ff",
+    },
+  },
+  instagramButton: {
+    ...baseLinkButton,
+    color: "#FF0069",
+    backgroundColor: "#ffe6f0",
+    "&:hover": {
+      backgroundColor: "#ffb8d4",
+    },
   },
 };
 
