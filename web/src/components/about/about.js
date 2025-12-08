6938f65de20f9c0b3f4b1a1f6aedf7b266324a3f
Jessee Zhang <jesseelzhang@gmail.com>
2025-12-07 22:37:34 -0800

    Creates v1 of the about section

--- web/src/components/about/about.js
+++ web/src/components/about/about.js
@@ -1,5 +1,118 @@
+import { Box, Container, Typography } from "@mui/material";
+import AboutUsSplash from "assets/images/about-us-splash.jpg";
+import ImageOfRiley from "assets/images/riley-headshot.jpg";
+import aboutStyles from "./about.styles";
+import messages from "./messages";
+import DyosLink from "components/common/link";
+import { styled } from "@mui/material/styles";
+
+const Paragraph = styled("p")(({ _ }) => aboutStyles.paragraph);
+
+function BioText() {
+  function createPiece(piece) {
+    if (!!piece.link) {
+      return (
+        <DyosLink href={piece.link} sx={aboutStyles.textLink} openInNewTab>
+          {piece.text}
+        </DyosLink>
+      );
+    }
+    return (
+      <Typography display="inline" sx={aboutStyles.text}>
+        {piece.text}
+      </Typography>
+    );
+  }
+
+  function createParagraph(paragraph) {
+    return (
+      <Paragraph variant="body1">{paragraph.pieces.map(createPiece)}</Paragraph>
+    );
+  }
+
+  return <Box>{messages.rileyBio.paragraphs.map(createParagraph)}</Box>;
+}
+
+function CliftonStrengths() {
+  let comma = (
+    <Typography display="inline" sx={aboutStyles.textLink}>
+      {messages.strengthsDelimiter}
+    </Typography>
+  );
+
+  function createStrength(strength) {
+    return (
+      <DyosLink href={strength.link} openInNewTab sx={aboutStyles.textLink}>
+        {strength.name}
+      </DyosLink>
+    );
+  }
+
+  let strengths = messages.rileyBio.cliftonStrengths;
+  let strengthsSection = [];
+
+  for (var i in strengths) {
+    strengthsSection.push(createStrength(strengths[i]));
+
+    if (i < strengths.length - 1) {
+      strengthsSection.push(comma);
+    }
+  }
+
+  return (
+    <Box>
+      <Typography variant="h6">{messages.cliftonStrengths}</Typography>
+      {strengthsSection}
+    </Box>
+  );
+}
+
 function About() {
-  return <div>About!</div>;
+  return (
+    <Box>
+      <Box sx={aboutStyles.splashContainer}>
+        <Box
+          component="img"
+          src={AboutUsSplash}
+          sx={aboutStyles.splash}
+          alt={messages.aboutUsSplashDescription}
+        />
+        <Box sx={aboutStyles.splashTextOverlayContainer}>
+          <Typography variant="h2" sx={aboutStyles.title}>
+            {messages.title}
+          </Typography>
+          <Typography variant="subtitle" sx={aboutStyles.subtitle}>
+            {messages.subtitle}
+          </Typography>
+        </Box>
+      </Box>
+      <Box sx={aboutStyles.contentContainer}>
+        <Container>
+          <Box
+            component="img"
+            src={ImageOfRiley}
+            sx={aboutStyles.rileyHeadshot}
+            alt={messages.rileyHeadshotDescription}
+          />
+          <Box>
+            <Box sx={aboutStyles.rileySectionTitleContainer}>
+              <Typography variant="h3" sx={aboutStyles.rileySectionTitle}>
+                {messages.rileySectionTitle}
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                sx={aboutStyles.rileySectionSubtitle}
+              >
+                {messages.rileySectionSubtitle}
+              </Typography>
+            </Box>
+            <BioText />
+            <CliftonStrengths />
+          </Box>
+        </Container>
+      </Box>
+    </Box>
+  );
 }
 
 export default About;
---
+++ web/src/components/about/messages.js
@@ -0,0 +1,130 @@
+const messages = {
+  aboutUsSplashDescription:
+    "Students and staff of Do Your Own Swing posing merrily in front of a wall displaying Studio M",
+  title: "About Us",
+  subtitle: "We're excited to get to know you",
+  rileySectionTitle: "Hi, I'm Riley!",
+  rileySectionSubtitle: "(They / Them)",
+  rileyHeadshotDescription:
+    "A headshot of Riley, the founder and teacher of Do Your Own Swing",
+  cliftonStrengths: "Clifton Strengths: ",
+  strengthsDelimiter: ", ",
+  rileyBio: {
+    paragraphs: [
+      {
+        pieces: [
+          {
+            text:
+              "I started my West Coast Swing journey 16 years ago, " +
+              "when I fell in love with the dance at my first class taught " +
+              "by the Joel Gibson in New Zealand. I'm lucky to have watched " +
+              "this dance evolve over the years, with a front " +
+              "row seat as a competitor, and as a teacher. I'm so excited " +
+              "to be able to share my passion for West Coast Swing with you all.",
+          },
+        ],
+      },
+
+      {
+        pieces: [
+          {
+            text:
+              "I founded Do Your Own Swing in 2016, though I never expected " +
+              "to see it grow into such a tight knit and warm community. When " +
+              "I started the class and social, I wanted to create a welcoming " +
+              "environment where everyone felt like they belonged, something " +
+              "that a young Riley would love to have been a part of. Fast forward " +
+              "to today, the class and social are still going strong, and DYOS is " +
+              "a pillar of West Coast Swing in the Bay Area.",
+          },
+        ],
+      },
+
+      {
+        pieces: [
+          {
+            text:
+              "In addition to running Do Your Own Swing and teaching the classes, " +
+              "I also run the ",
+          },
+          {
+            text: "Explore Community Space",
+            link: "https://fb.com/ExploreWCS",
+          },
+
+          {
+            text:
+              " in Oakland on Mondays. On the weekends when I go to conventions, I " +
+              "still continue to compete in the improvised and routine divisions, " +
+              "teach workshops, and give private lessons. But you'll often see me running the ",
+          },
+          {
+            text: "Swing Reports and Restorations program",
+            link: "https://sites.google.com/view/swing-reports-restorations/",
+          },
+          {
+            text:
+              ", which I started and designed to increase safety at West Coast " +
+              "Swing events by providing policy consultation and development along " +
+              "with incident response plans.",
+          },
+        ],
+      },
+
+      {
+        pieces: [
+          {
+            text:
+              "West Coast Swing is a huge part of my life, and I love to be able " +
+              "to teach and grow dancers, and more importantly, foster a community " +
+              "where everyone feels included and safe. I hope to see you next Thursday!",
+          },
+        ],
+      },
+    ],
+    cliftonStrengths: [
+      {
+        name: "Ideation",
+        link: "https://www.gallup.com/cliftonstrengths/en/252260/ideation-theme.aspx",
+      },
+      {
+        name: "Connectedness",
+        link: "https://www.gallup.com/cliftonstrengths/en/252197/connectedness-theme.aspx",
+      },
+      {
+        name: "Adaptability",
+        link: "https://www.gallup.com/cliftonstrengths/en/252146/adaptability-theme.aspx",
+      },
+      {
+        name: "Relator",
+        link: "https://www.gallup.com/cliftonstrengths/en/252311/relator-theme.aspx",
+      },
+      {
+        name: "Individualization",
+        link: "https://www.gallup.com/cliftonstrengths/en/252272/individualization-theme.aspx",
+      },
+      {
+        name: "Input",
+        link: "https://www.gallup.com/cliftonstrengths/en/252278/input-theme.aspx",
+      },
+      {
+        name: "Intellection",
+        link: "https://www.gallup.com/cliftonstrengths/en/252284/intellection-theme.aspx",
+      },
+      {
+        name: "Belief",
+        link: "https://www.gallup.com/cliftonstrengths/en/252170/belief-theme.aspx",
+      },
+      {
+        name: "Strategic",
+        link: "https://www.gallup.com/cliftonstrengths/en/252350/strategic-theme.aspx",
+      },
+      {
+        name: "Empathy",
+        link: "https://www.gallup.com/cliftonstrengths/en/252236/empathy-theme.aspx",
+      },
+    ],
+  },
+};
+
+export default messages;
