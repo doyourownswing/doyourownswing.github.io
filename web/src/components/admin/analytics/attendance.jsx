6685cb2b75961d661c07dee5e76ff227d2337ccf
Ryan Guan <rzgn@users.noreply.github.com>
2026-07-22 23:12:30 -0700

    Add summary stats for attendance numbers

--- web/src/components/admin/analytics/attendance.jsx
+++ web/src/components/admin/analytics/attendance.jsx
@@ -1,9 +1,91 @@
 import theme from "@/common/theme";
 import analyticsStyles from "@/components/admin/analytics/analytics.styles";
-import { prettyPrintDate } from "@/components/admin/analytics/utils";
+import { prettyPrintDate, shortDate } from "@/components/admin/analytics/utils";
 import DyosCard from "@/components/common/card";
 import { Box, Grid, Typography } from "@mui/material";
 import { BarChart, LineChart } from "@mui/x-charts";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+
+/**
+ * Given dataArrays (an array of numeric arrays),
+ * dates (an array of strings with the same length as an array in data),
+ * labels (one for each array in data),
+ * and colors (optional)
+ * displays summary statistics for each array in the data.
+ */
+function SummaryStats({ dataArrays, dates, labels }) {
+  // helper for calculating median (can also be used for quartiles)
+  const getElemAroundIndex = (arr, i) => {
+    if (i === Math.trunc(i)) {
+      return arr[i];
+    } else {
+      return (arr[Math.floor(i) - 1] + arr[Math.floor(i)]) / 2;
+    }
+  };
+
+  const summaries = dataArrays.map((arr) => {
+    const sum = arr.reduce((acc, num) => acc + num, 0);
+    const mean = Math.round(sum / arr.length);
+
+    const sortedData = arr.toSorted((a, b) => Number(a) - Number(b));
+    const median = getElemAroundIndex(sortedData, arr.length / 2);
+
+    const max = arr.reduce((a, b) => Math.max(a, b), -Infinity);
+    const maxIdx = arr.indexOf(max);
+    const min = arr.reduce((a, b) => Math.min(a, b), Infinity);
+    const minIdx = arr.indexOf(min);
+
+    return { mean, median, max, min, maxIdx, minIdx, sortedData };
+  });
+
+  return (
+    <details>
+      <summary>Show/hide summary stats</summary>
+      <LineChart
+        height={300}
+        series={summaries.map(({ sortedData }, i) => {
+          return { data: sortedData, label: `${labels[i]} (sorted)` };
+        })}
+        yAxis={[{ min: 0 }]}
+        margin={{ right: 64 }}
+      />
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>Min</TableCell>
+            <TableCell>Avg</TableCell>
+            <TableCell>Median</TableCell>
+            <TableCell>Max</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {summaries.map(({ mean, median, max, min, maxIdx, minIdx }, i) => (
+            <TableRow>
+              <TableCell>{labels[i]}</TableCell>
+              <TableCell>
+                {min}
+                <br />({shortDate(dates[minIdx])})
+              </TableCell>
+              <TableCell>{mean}</TableCell>
+              <TableCell>{median}</TableCell>
+              <TableCell>
+                {max}
+                <br />({shortDate(dates[maxIdx])})
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </details>
+  );
+}
 
 function TotalWeeklyAttendance(props) {
   let data = props.data.weeklyStats;
@@ -25,6 +107,11 @@ function TotalWeeklyAttendance(props) {
           xAxis={[{ scaleType: "point", data: xLabels, height: 28 }]}
           yAxis={[{ min: 0 }]}
           margin={{ right: 64 }}
+        />
+        <SummaryStats
+          dataArrays={[weeklyAttendance]}
+          dates={data.map((d) => d.date)}
+          labels={["Total attendees"]}
         />
       </DyosCard>
     </Grid>
@@ -69,6 +156,11 @@ function ClassAttendance(props) {
           colors={colors}
           xAxis={[{ data: xLabels, height: 28 }]}
           yAxis={[{ width: 50 }]}
+        />
+        <SummaryStats
+          dataArrays={[l1, l2, l3, l4, socialOnly]}
+          dates={data.map((d) => d.date)}
+          labels={["L1", "L2", "L3", "L4", "Social"]}
         />
       </DyosCard>
     </Grid>
